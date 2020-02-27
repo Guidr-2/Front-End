21658fb96e39bb253d965ede07be24f03182bad2
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-02-27 00:08:05 -0800

    Login Form Done

---
+++ guidr/src/component/Login.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import * as Yup from 'yup';
+// import axios from 'axios';
+
+import { withFormik, Form, Field } from 'formik';
+
+const Login = ({ values, errors, touched }) => {
+  console.log(values.isSubmitting);
+  return (
+    <Form>
+      <Field type='text' name='username' />
+      {touched.username && errors.username && (
+        <p className='errors'>{errors.username}</p>
+      )}
+      <Field type='password' name='password' />
+      {touched.password && errors.password && (
+        <p className='errors'>{errors.password}</p>
+      )}
+      <button type='submit' disabled={values.isSubmitting}>
+        {values.isSubmitting ? 'Submitting' : 'Submit'}
+      </button>
+    </Form>
+  );
+};
+
+export default withFormik({
+  mapPropsToValues: props => ({
+    username: '',
+    password: ''
+  }),
+  validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .min(2, 'Too Short!')
+      .max(70, 'Too Long!')
+      .required('UserName is Required!'),
+    password: Yup.string()
+      .min(6, 'Too Short!')
+      .max(70, 'Too Long!')
+      .required('Password is Required')
+  }),
+  handleSubmit: (values, { setSubmitting, resetForm }) => {
+    // axios
+    //   .get('', values)
+    //   .then(response => {
+    //   })
+    //   .catch(err => console.log(err.response));
+
+    setTimeout(() => {
+      console.log(values);
+      setSubmitting(false);
+    }, 1000);
+    resetForm();
+  }
+})(Login);
---
+++ guidr/src/component/Trips.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Trips() {
+  return <div className='Trips'></div>;
+}
+
+export default Trips;
