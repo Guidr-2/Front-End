9f32761320b31e1edc0a36926cbd974f6000eb31
Robert Kulp <pedalinon@gmail.com>
2020-03-09 14:05:49 -0700

    finally maybe

---
+++ build/precache-manifest.a7475aaefa8cf4290cb6b44949cc84cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c8363ba1d67e5be7c0f39c8ed61cd6",
+    "url": "/Front-End/public/index.html"
+  },
+  {
+    "revision": "b39de779021ea0b616c6",
+    "url": "/Front-End/public/static/css/main.bbe161e4.chunk.css"
+  },
+  {
+    "revision": "1af717b444ed2734cab2",
+    "url": "/Front-End/public/static/js/2.cfe1b058.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/Front-End/public/static/js/2.cfe1b058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39de779021ea0b616c6",
+    "url": "/Front-End/public/static/js/main.46cf68a8.chunk.js"
+  },
+  {
+    "revision": "a39719862ba266830017",
+    "url": "/Front-End/public/static/js/runtime-main.6e558f0e.js"
+  }
+]);
